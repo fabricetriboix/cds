4e01f84a3f358bb7b2d71642f48f8f4d1247c7c5
Fabrice Triboix <ftriboix@gmail.com>
2016-12-01 17:19:27 +0000

    Move from scons to make - 1st step: build the library

---
+++ Makefile
@@ -0,0 +1,89 @@
+# Copyright (c) 2016  Fabrice Triboix
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+
+###  BEGIN CONFIGURATION PARAMETERS  ###
+
+# **VERY IMPORTANT**: This makefile assumes all source files have different
+# names. This is because all object files go to a single build directory. Also
+# ensure that no directory or file name has blank in them.
+
+# Set D to 1 to increase verbosity
+D = 0
+
+# Set V to "release" to make a release build
+# Supported variants: "debug" (default) and "release"
+V = debug
+
+# Set PLF to the platform you want to build to
+# This must be one of the platform directory listed under "src/rtplf"
+PLF := $(shell ./autodetectplf.py)
+
+CC = gcc
+CXX = g++
+AR = ar
+DEFS = -D_GNU_SOURCE
+CFLAGS = -Wall -Wextra -Werror -std=c99
+CXXFLAGS = -Wall -Wextra -Werror -std=c++11
+LINKFLAGS =
+
+ifneq ($(V),debug)
+CFLAGS += -O3
+CXXFLAGS += -O3
+else
+DEFS += -DCDS_WITH_FLLOC
+CFLAGS += -O0 -g
+CXXFLAGS += -O0 -g
+endif
+
+CFLAGS += $(DEFS)
+CXXFLAGS += $(DEFS)
+
+# Default installation PREFIX
+PREFIX = /usr/local
+
+# Installation directories
+LIBDIR = $(PREFIX)/lib
+INCDIR = $(PREFIX)/include
+DOCDIR = $(PREFIX)/doc/cds
+
+###  END CONFIGURATION PARAMETERS  ###
+
+
+
+###  DO NOT MODIFY ANYTHIG BELOW THIS LINE  ###
+
+BUILDDIR = build/$(PLF)/$(V)
+TOPDIR = ../../..
+
+# Export all variables
+export
+
+# Check there are no duplicate source file names
+count1 := $(shell find src -name '*.c' | sed -e 's:.*/::' | sort | wc -l)
+count2 := $(shell find src -name '*.c' | sed -e 's:.*/::' | sort | uniq | wc -l)
+ifneq ($(count1),$(count2))
+$(error Duplicate source file names detected)
+endif
+
+all doc test install dbg:
+	@mkdir -p $(BUILDDIR) && $(MAKE) -C $(BUILDDIR) -f $(TOPDIR)/make.mk $@
+
+clean:
+	rm -rf $(BUILDDIR)
+
+# Helper targets
+debug release:
+	@$(MAKE) -C . V=$@ all
---
+++ make.mk
@@ -0,0 +1,207 @@
+# Copyright (c) 2016  Fabrice Triboix
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+SHELL := /bin/sh
+
+# Check existence of tools
+CCACHE := $(shell which ccache 2> /dev/null)
+DOXYGEN := $(shell which doxygen 2> /dev/null)
+DOT := $(shell which dot 2> /dev/null)
+
+MODULES = $(TOPDIR)/src/plf/$(PLF) $(TOPDIR)/src/list \
+			$(TOPDIR)/src/binarytree $(TOPDIR)/src/map
+
+# Path for make to search for source files
+VPATH = $(foreach i,$(MODULES),$(i)/src) $(foreach i,$(MODULES),$(i)/test)
+
+# Output libraries
+OUTPUT_LIBS = libcds.a
+
+# Include paths for compilation
+INCS = $(foreach i,$(MODULES),-I$(i)/include)
+
+# List public header files
+HDRS = $(foreach i,$(MODULES),$(wildcard $(i)/include/*.h))
+
+# List of object files for various targets
+LIBCDS_OBJS = cdscommon.o cdslist.o cdsbinarytree.o cdsmap.o
+RTTEST_MAIN_OBJ = rttestmain.o
+CDS_TEST_OBJS = test-list.o test-binarytree.o test-map.o
+
+
+# Standard targets
+
+# XXX all: $(OUTPUT_LIBS) rttest_unit_tests rtsys_unit_tests doc
+all: $(OUTPUT_LIBS)
+
+doc: doc/html/index.html
+
+
+# Rules to build object files, libraries, programs, etc.
+
+define RUN_CC
+set -eu; \
+cmd="$(CCACHE) $(CC) $(CFLAGS) $(INCS) -o $(1) -c $(2)"; \
+if [ $(D) == 1 ]; then \
+	echo "$$cmd"; \
+else \
+	echo "CC    $(1)"; \
+fi; \
+$$cmd
+endef
+
+define RUN_CXX
+set -eu; \
+cmd="$(CCACHE) $(CXX) $(CXXFLAGS) $(INCS) -o $(1) -c $(2)"; \
+if [ $(D) == 1 ]; then \
+	echo "$$cmd"; \
+else \
+	echo "CXX   $(1)"; \
+fi; \
+$$cmd
+endef
+
+define RUN_AR
+set -eu; \
+cmd="$(AR) crs $(1) $(2)"; \
+if [ $(D) == 1 ]; then \
+	echo "$$cmd"; \
+else \
+	echo "AR    $(1)"; \
+fi; \
+$$cmd
+endef
+
+define RUN_LINK
+set -eu; \
+cmd="$(CC) $(LINKFLAGS) -o $(1) $(2) -L. $(3)"; \
+if [ $(D) == 1 ]; then \
+	echo "$$cmd"; \
+else \
+	echo "LINK  $(1)"; \
+fi; \
+$$cmd
+endef
+
+libcds.a: $(LIBCDS_OBJS)
+
+cds_unit_tests: $(CDS_TEST_OBJS) $(RTTEST_MAIN_OBJ) $(OUTPUT_LIBS)
+	@$(call RUN_LINK,$@,$^,-lrttest -lrtsys)
+
+# TODO cdslistperf:
+# TODO stllistperf:
+# TODO cdsmapperf:
+# TODO stlmapperf:
+
+
+doc/html/index.html: $(HDRS)
+ifeq ($(DOXYGEN),)
+	@echo "Doxygen not found, documentation will not be built"
+else
+	@set -eu; \
+	if [ -z "$(DOT)" ]; then \
+		cp $(TOPDIR)/Doxyfile doxy1; \
+	else \
+		cat $(TOPDIR)/Doxyfile | sed -e 's/HAVE_DOT.*/HAVE_DOT = YES/' > doxy1;\
+	fi; \
+	cat doxy1 | sed -e 's:INPUT[	 ]*=.*:INPUT = $^:' > doxy2; \
+	cmd="doxygen doxy3"; \
+	if [ $(D) == 1 ]; then \
+		cp doxy2 doxy3; \
+		echo "$$cmd"; \
+	else \
+		cat doxy2 | sed -e 's/QUIET.*/QUIET = YES/' > doxy3; \
+		echo "DOXY  $@"; \
+	fi; \
+	$$cmd
+endif
+
+test: cds_unit_tests
+	@set -eu; \
+	if ! which rttest2text.py > /dev/null 2>&1; then \
+		echo "ERROR: rttest2text.py not found in PATH"; \
+		exit 1; \
+	fi; \
+	./$< > cds.rtt; \
+	find $(TOPDIR) -name 'test-*.c' | xargs rttest2text.py cds.rtt
+
+
+define INSTALL
+set -eu; \
+[ -x $(1) ] && mode=755 || mode=644; \
+dst=$(2)/`basename $(1)`; \
+cmd="mkdir -p `dirname $$dst` && cp -rf $(1) $$dst && chmod $$mode $$dst"; \
+if [ $(D) == 1 ]; then \
+	echo "$$cmd"; \
+else \
+	echo "INST  $(1)"; \
+fi; \
+eval $$cmd;
+endef
+
+
+install: install_lib install_inc install_doc
+
+install_lib: $(OUTPUT_LIBS)
+	@$(foreach i,$^,$(call INSTALL,$(i),$(LIBDIR)))
+
+install_inc:
+	@$(foreach i,$(HDRS),$(call INSTALL,$(i),$(INCDIR)))
+
+install_doc: doc
+	@$(call INSTALL,doc/html,$(DOCDIR))
+
+
+lib%.a:
+	@$(call RUN_AR,$@,$^)
+
+%.o: %.c
+	@$(call RUN_CC,$@,$<)
+
+%.o: %.cpp
+	@$(call RUN_CXX,$@,$<)
+
+dbg:
+	@echo "Platform = $(PLF)"
+	@echo "VPATH = $(VPATH)"
+	@echo "HDRS = $(HDRS)"
+
+
+# Automatic header dependencies
+
+# XXX OBJS = $(LIBCDS_OBJS) $(RTTEST_MAIN_OBJ) $(CDS_TEST_OBJS)
+OBJS = $(LIBCDS_OBJS)
+
+-include $(OBJS:.o=.d)
+
+%.d: %.c
+	@set -eu; \
+	cmd="$(CC) $(CFLAGS) $(INCS) -MT $(@:.d=.o) -MM -MF $@ $<"; \
+	if [ $(D) == 1 ]; then \
+		echo "$$cmd"; \
+	else \
+		echo "DEP   $@"; \
+	fi; \
+	$$cmd
+
+%.d: %.cpp
+	@set -eu; \
+	cmd="$(CXX) $(CXXFLAGS) $(INCS) -MT $(@:.d=.o) -MM -MF $@ $<"; \
+	if [ $(D) == 1 ]; then \
+		echo "$$cmd"; \
+	else \
+		echo "DEPXX $@"; \
+	fi; \
+	$$cmd
